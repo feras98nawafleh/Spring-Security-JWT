6be82b46ff373f1ad4d316e72b0add1254099155
feras98nawafleh <feras.a.nawafleh@gmail.com>
2022-06-02 11:03:11 +0300

    initial commit

---
+++ src/test/java/feras/nawafleh/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package feras.nawafleh.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
